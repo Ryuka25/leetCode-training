61abb133d6a904b67e0ea77269127dabe947b3f4
Ryuka25 <lovanirina.ran@gmail.com>
2021-08-21 00:37:02 +0300

    FINISH easy/python/resolution_fonction_dans_R.py

--- easy/python/resolution_fonction_dans_R.py
+++ easy/python/resolution_fonction_dans_R.py
@@ -1,5 +1,7 @@
 #! usr/bin/python3
 
+import math as mt
+
 print("""
 /********************************************/
 /*  ____   __   __  _   _   _  __     _     */
@@ -10,20 +12,62 @@ print("""
 /*                                          */
 /********************************************/
 """)
-print("\nThis program allow user to resolve an 2nd degree function ( ax^2 + bx + c ) in R\n")
-print("\n[*] Begin script ... ")
+print("This program allow user to resolve an 2nd degree function ( ax² + bx + c ) in R\n")
+print("[*] Begin script ... ")
 
-a = float(input("\nEnter the 'a' value \n> "))
-b = float(input("\nEnter the 'b' value \n> "))
-c = float(input("\nEnter the 'c' value \n> "))
+a = float(input("Enter the 'a' value \n> "))
+b = float(input("Enter the 'b' value \n> "))
+c = float(input("Enter the 'c' value \n> "))
 
-delta = (b*b) - 4*(a*c)
+print(f"Your functin is like: {a}x² + {b}x + {c} ")
+
+delta = (pow(b,2)) - 4*(a*c)
 print("[*] Calculating delta ...")
 print(f"[*] Delta value is {delta}")
 
 if (delta > 0):
+
+    sqrtDelta = mt.sqrt(delta) # Calculate the square root of Delta
+
     print("[*] Delta is greater than zero")
+    solutions = [
+        (-b + sqrtDelta) / (2*a),
+        (-b - sqrtDelta) / (2*a)
+    ]
 
 # TODO: @Ryuka25 Finish this now!
 
-print ("\n[*] End of script !")
+elif (delta == 0):
+    
+    sqrtDelta = mt.sqrt(delta) # Calculate the square root of Delta
+
+    print("[*] Delta is equal to zero")
+    solutions = [
+        (-b + sqrtDelta) / (2*a)
+    ]
+
+elif (delta < 0):
+    print("[*] Delta is less than zero")
+    solutions = [
+        "No solution in R"
+    ]
+else:
+    print("[*] FATAL ERROR OCCURED, this programs will end!")
+
+print(f"[*] {len(solutions)} Solution was found ...")
+
+print("""
+/*******************/
+/* Begin Solutions */
+/*******************/
+""")
+
+print(solutions)
+
+print("""
+/*****************/
+/* End Solutions */
+/*****************/
+""")
+
+print ("[*] End of script !")
