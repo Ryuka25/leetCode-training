d2a62e60f7b57991719aeab7155496f724a50390
Ryuka25 <lovanirina.ran@gmail.com>
2021-08-27 20:32:50 +0300

    Update v27.08.21
    Adding reply to question 1,2,3,4,5 of Exercice Algo

--- easy/python/mathematical/00_sum_first_integer.py
+++ easy/python/mathematical/00_sum_first_integer.py
@@ -1,3 +1,93 @@
 #!/usr/bin/env python
 
-# Ecrire un algorithme qui calcule la somme de 𝑛 premiers entiers.
+import math
+
+def question1():
+    
+    # Ecrire un algorithme qui calcule la somme de n premiers entiers.
+
+    def npremier(nombre:int)-> list:
+        result = [1] # Le premier nombre premier est 1 est non pas 0
+        for i in range(1,nombre):
+            actual = i
+            result.append(actual + (i+1))
+
+        return result
+
+    result = npremier(5)
+    print(result)
+
+def question2():
+
+    # Ecrire un algorithme qui calcule le factoriel de n.
+
+    math.factorial(4)
+
+    def factoriel(nombre:int)->int:
+        if (nombre == 0):
+            result = 1
+        else:
+            result = nombre * factoriel(nombre-1)
+
+        return result
+    
+    result = factoriel(4)
+    print(result)
+
+
+def question3():
+
+    # Ecrire un algorithme qui calcule le monôme pow(X,n).
+
+    math.pow(2,3) # 2**3 == pow(2,3)
+
+    def puissance(nombre:float, puissance:int)->float:
+        original = nombre
+        for i in range(puissance-1):
+            nombre = original * nombre
+
+        return nombre
+
+    result = puissance(2,3)
+    print(result)
+
+
+def question4():
+
+    # Ecrire un algorithme qui calcule le factoriel de 𝐶 𝑝𝑛 .
+
+    math.comb(4,2)
+
+    def combinaison(n:int,p:int,)->float:
+        # math.comb(n,p) # n:items dans le sac p:items à piocher
+        result = (math.factorial(n)) / (math.factorial(p)*(math.factorial(n-p)))
+        
+        return result
+
+    result=combinaison(4,2)
+    print(result)
+
+def question5():
+
+    # Ecrire un algorithme pour trouver le terme général de la suite:
+    # 𝑈 𝑛 = 2 · 𝑈 𝑛−1 + 1, où
+    # 𝑈 0 = 1
+
+    def U(n:int)->int:
+        result=(0)
+        if (n == 0):
+            result=(1)
+        else:
+            result=((2*U(n-1))+1)
+        
+        return result
+
+    result = U(1)
+    print(result)
+
+def main():
+    question5()
+    return 0
+
+if (__name__ == '__main__'):
+    main()
