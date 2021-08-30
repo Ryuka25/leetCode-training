e32caa47e6f7a5218259bff366905d1906350eab
Ryuka25 <lovanirina.ran@gmail.com>
2021-08-30 15:12:33 +0300

    Update Pattern "easy/python/simpleCalculator.py"

--- easy/python/simpleCalculator.py
+++ easy/python/simpleCalculator.py
@@ -9,59 +9,85 @@ print("""
 
                     - by Ryuka (2021)
  """)
+ 
+class calculator:
 
-def getNumbers():
-    global number1
-    global number2
-    number1 = float(input("Enter the first number\t: "))
-    number2 = float(input("Enter the second number\t: "))
+    def getOperator(self):
+        choosing = True
+        while choosing:
+            self.operator = int(input("Please enter a number for the operation: "))
+            if (self.operator == 1):
+                self.operatorSign = "+"
+                break
+            elif (self.operator == 2):
+                self.operator = "-"
+                break
+            elif (self.operator == 3):
+                self.operatorSign = "*"
+                break
+            elif (self.operator == 4):
+                self.operatorSign = "/"
+                break
+            else:
+                print("[*] ERROR ! Please choose a correct number for the operation ...")
+                pass
 
-def addition(number1, number2):
-    print("[*] Process addition")
-    global result
-    result = number1 + number2
-    print ("[*] Result Found ")
+    def getNumbers(self):
+        self.number1 = float(input("Enter the first number\t: "))
+        self.number2 = float(input("Enter the second number\t: "))
 
-def subtraction(number1, number2):
-    print("[*] Process soustraction")
-    global result
-    result = number1 - number2
-    print ("[*] Result Found ")
+    def addition(self):
+        self.result = self.number1 + self.number2
 
-def multiplication(number1, number2):
-    print("[*] Process multiplication")
-    global result
-    result = number1 * number2
-    print ("[*] Result Found ")
+    def subtraction(self):
+        self.result =  self.number1 - self.number2
 
-def division(number1, number2):
-    print("[*] Process division")
-    global result
-    result = number1 / number2
-    print ("[*] Result Found ")
+    def multiplication(self):
+        self.result = self.number1 * self.number2
 
+    def division(self):
+        self.result = self.number1 / self.number2
+
+    def showResult(self):
+        print(f"[*] Result of operation : ' {self.number1} {self.operatorSign} {self.number2} = {self.result}' ")
+
+    def execute(self):
+        if (self.operator == 1):
+            self.addition()
+        elif (self.operator == 2):
+            self.subtraction()
+        elif (self.operator == 3):
+            self.multiplication()
+        elif (self.operator == 4):
+            self.division()
+
+class programmsBanner:
+
+    def __init__(self):
+        self.content = "[*] This programms allows user to make a binary operations with:\n\t1- Addition\n\t2- Subtraction\n\t3- Multiplication\n\t4- Division"
+
+    def show(self):
+        print(self.content)
 
 def main():
+    banner = programmsBanner()
+    calc = calculator()
+    run = True
+
     print("[*] Begin of script")
-    print("[*] This programms allows user to make a binary operations with:\n\t1- Addition\n\t2- Subtraction\n\t3- Multiplication\n\t4- Division")
-    operator = int(input("Please enter a number for the operation: "))
 
-    getNumbers()
+    banner.show()
+    while run:
+        calc.getNumbers()
+        calc.getOperator()
+        calc.execute()
+        calc.showResult()
+        reRun = (input("[!] Would you like to re-use this calculator ? (y/n): ")).lower()
+        if (reRun in ["yes", "y"]):
+            pass
+        elif (reRun in ["no","n"]):
+            break
 
-    if (operator == 1):
-        addition(number1, number2)
-    elif (operator == 2):
-        subtraction(number1, number2)
-    elif (operator == 3):
-        multiplication(number1, number2)
-    elif (operator == 4):
-        division(number1, number2)
-    else:
-        print("[*] ERROR ! Please choose a correct number for the operation ...")
-        exit()
-
-    print("[*] Process finished")
-    print(f"[*] Result of operation between {number1} and {number2} is {result}")
     print("[*] End of script")
     
 # Cette bout de code permet de choisir la fonction main()
