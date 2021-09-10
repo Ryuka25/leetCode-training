573bcbc7f03b888b2412b4ae554229a9c470d538
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-10 06:20:24 +0300

    ♻️ UPDATE `.../find_number.py` : Add Game._validNumber()

--- easy/python/exercices/find_number.py
+++ easy/python/exercices/find_number.py
@@ -5,5 +5,22 @@ class Game:
     def _createRandomNumber()->int:
         "Return a random number between [0;100]"
         number = randint(a=0,b=100)
-        
+
         return number
+
+    def _validNumber(self, number:int)->bool:
+        "Make sure that we have a valid number"
+        try:
+            number = int(number)
+            if (number < 0):
+                result = False
+                print("\n[!] Tips : Number should be greater or equalt to 0")
+            else:
+                result = True
+                print("[+] Number is conform to a typical response")
+                
+        except:
+            print("\n[!] Only Integer number are allowed")
+            result = False
+
+        return result
