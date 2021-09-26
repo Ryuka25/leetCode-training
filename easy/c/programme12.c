eca8d3261760ce7d9da412df2a5f0706bede17df
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-26 17:49:42 +0300

    📦️ ADD `.../programme12.c`

---
+++ easy/c/programme12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char C;
+    char o;
+    char u;
+    char c;
+
+    C = 'C';
+    c = 'c';
+    o = 'o';
+    u = 'u';
+
+    int number;
+    number = 17;
+
+    printf("\n%c%c%c%c%c%c", C,o,u,c,o,u);
+    printf("\n%d",number);
+    getchar();
+    return 0;
+}
