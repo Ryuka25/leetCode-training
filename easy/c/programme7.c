0b324fcd8915cc9d237329cc025e1546a026e694
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-26 16:48:17 +0300

    📦️ ADD `easy/c/programme7.c`

---
+++ easy/c/programme7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Coucou c'est moi\n");
+    /* Fonction indispensable pour l'affichage des variables */
+
+    getchar();
+    return 0;
+}
