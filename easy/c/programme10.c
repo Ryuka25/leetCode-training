5742793e6d2899aa9c66acc08d806e7356ce7f80
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-26 17:26:00 +0300

    📦️ ADD `.../programme10.c` : Variable INT

---
+++ easy/c/programme10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num_1;
+    int num_2;
+    int num_3;
+    int num_4;
+
+    num_1 = 70;
+    num_2 = 82;
+    num_3 = 185;
+    num_4 = 30;
+
+    printf("%d, %d, %d, %d", num_1, num_2, num_3, num_4);
+
+    getchar();
+    return 0;
+}
