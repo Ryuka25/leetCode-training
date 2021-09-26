bc61ec7ede25553df3834ae914ceceef8186b456
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-26 17:26:29 +0300

    📦️ ADD `.../programme11.c` : Variable CHAR

---
+++ easy/c/programme11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char c;
+    char o;
+    char u;
+    char C;
+    char O;
+    char U;
+    
+    c = 'c';
+    o = 'o';
+    u = 'u';
+    C = 'C';
+    O = 'O';
+    U = 'U';
+
+    printf("%c%c%c%c%c%c", c,o,u,C,O,U);
+    getchar();
+    return 0;
+}
