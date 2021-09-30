36b49550e9a414f810e24e8ca89e628f0b68f657
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-30 03:20:18 +0300

    📦️ ADD `c` | `add_two_integers`

---
+++ easy/c/learn_c_by_example/add_two_integers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int nb1,nb2;
+
+    printf("Entrez deux nombres : ");
+    scanf("%d %d", &nb1, &nb2);
+    printf("le Résultat de l'addition est : %d", (nb1 + nb2));
+    
+    return 0;
+}
