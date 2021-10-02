2456df87f58ed41f116fc2e7700074c9b973a800
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-02 10:46:45 +0300

    🚚️ RENAME `easy` to `general`

--- easy/php/mvc/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-define ('ROOT', '/');
-define('APP_ROOT', ROOT.'mvc/');
-define('ALL_PAGES', array('home'=>APP_ROOT,'about'=>APP_ROOT.'about'));
--- easy/python/python-site-web-flask/models/config.py
+++
@@ -1,4 +0,0 @@
-DB_HOST = "localhost"
-DB_USER = "admin"
-DB_PASSWD = "adminpass"
-DB_NAME = "flask_database"
