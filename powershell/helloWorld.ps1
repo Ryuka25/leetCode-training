5d021aafe4f3bb3a493621109481e33237530a31
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-02 11:04:23 +0300

    ♻️ UPDATE `helloWorld`.ps1
    🎨️ REFACTOR `helloWorld.ps1`

--- powershell/helloWorld.ps1
+++ powershell/helloWorld.ps1
@@ -1,5 +1,8 @@
 #! /usr/bin/env pwsh
 
-Clear-Host                  # Clear the host terminal screen
-Write-Host "Hello World"    # Write "Str" in the host terminal screen
-Read-Host                   # Get any-text
+function HelloWorld {
+    Clear-Host                  # Clear the host terminal screen
+    Write-Host "Hello World"    # Write "Str" in the host terminal screen
+}
+
+HelloWorld
