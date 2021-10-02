b2b9f4a826c73f1f8cc7f5d2f9131950e4b6e653
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-02 11:00:18 +0300

    📦️ ADD `powershell/helloWorld.ps1`

---
+++ powershell/helloWorld.ps1
@@ -0,0 +1,5 @@
+#! /usr/bin/env pwsh
+
+Clear-Host                  # Clear the host terminal screen
+Write-Host "Hello World"    # Write "Str" in the host terminal screen
+Read-Host                   # Get any-text
