e9d5d4c13d95272f519e7c51d9b2b48748504018
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-04 03:23:06 +0300

    📦️ ADD `python/maths`

---
+++ python/maths/how_to_import.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:Utf-8 -*-
+
+############################
+# Importation des modules :
+
+from mathematical import *
+
+try:
+    from maths.mathematical import *
+except:
+    pass
+
+##########################
+# Programmes Principales :
+
+def main():
+    
+    return 0
+
+if (__name__ == '__main__'):
+    main()
