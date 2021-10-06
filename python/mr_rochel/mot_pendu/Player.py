b5fde14737d0204a639533a81089832c60c39bc0
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-06 14:06:51 +0300

    📦️ ADD `mot_pendu/Player.py`

--- python/mr_rochel/mot_pendu/Player.py
+++ python/mr_rochel/mot_pendu/Player.py
@@ -1,10 +1,25 @@
 class Player:
 
     def __init__(self,data):
-        self.name = data['name']
-        self.level = "easy"
-        self.numberOfCorrectAnswer = 0
+        self.__name = data['name']
+        self.__level = "easy"
+        self.__numberOfCorrectAnswer = 0
     
     def evaluatePlayerLevel(self):
-        if (self.numberOfCorrectAnswer <= 10):
-            self.level = 'easy'
+        if (self.__numberOfCorrectAnswer <= 10):
+            self.__level = 'easy'
+
+    def getLevel(self):
+        return self.__level
+
+    def getNumberOfCorrectAnswer(self):
+        return self.__numberOfCorrectAnswer
+
+    def getName(self):
+        return self.__name
+
+    def setNumberOfCorrectAnswer(self, value:int):
+        self.__numberOfCorrectAnswer = value
+    
+    def upNumberOfCorrectAnswer(self):
+        self.setNumberOfCorrectAnswer((self.getNumberOfCorrectAnswer() +1 ))
