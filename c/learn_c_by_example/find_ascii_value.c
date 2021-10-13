1771348d328be4ddf85dc7dd03e95c371117fdaf
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-13 04:06:29 +0300

    📦️ ADD `learn_c_by_example`

---
+++ c/learn_c_by_example/find_ascii_value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char charac;
+    printf("Enter a character: ");
+    scanf("%c", &charac);
+    printf("Ascii value is %d", charac);
+    
+    return 0;
+}
