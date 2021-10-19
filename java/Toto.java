732cf3aec86f726e8b0409cc94456db50e89ad38
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-20 01:55:46 +0300

    📦️ ADD : `Toto.java`, my helloWorld

---
+++ java/Toto.java
@@ -0,0 +1,14 @@
+/**
+* Ma premier essai
+* Affiche : “Hello Toto”
+* @author moi
+* @date juste_avant
+*/
+public class Toto {
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello Toto");
+    }
+}
